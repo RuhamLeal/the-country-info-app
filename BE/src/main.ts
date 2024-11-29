f55f4a17beac6d26fa5ece9f4202daf8acf9817b
Ruham Leal <ruhamxlpro@hotmail.com>
2024-11-29 20:05:35 -0300

    :wrench: config: Implementing fastify adapter

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: {
+        origin: '*',
+        allowedHeaders: '*',
+      },
+    },
+  );
+  await app.listen({
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+  });
 }
 bootstrap();
