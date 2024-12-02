42ca40377ee722300246d176a03aff1cf81c0357
Ruham Leal <ruhamxlpro@hotmail.com>
2024-12-01 23:09:28 -0300

    :wrench: config: Adding next app config

---
+++ FE/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+
+  reactStrictMode: true,
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
+};
+
+export default nextConfig;
