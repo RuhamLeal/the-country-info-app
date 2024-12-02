077a027affa9863eb86e7b0b085fbcc1a46b1325
Ruham Leal <ruhamxlpro@hotmail.com>
2024-12-02 02:43:43 -0300

    :sparkles: feat: Implementing redirect direct to /countries page

--- FE/next.config.ts
+++ FE/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/countries',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['pt-BR'],
