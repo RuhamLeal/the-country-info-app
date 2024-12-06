a5b6c9e6fbc67454242b8db5dccb334678516b72
Ruham Leal <ruhamxlpro@hotmail.com>
2024-12-06 08:27:30 -0300

    :hammer: refact: return Image component directly in country card component

--- FE/components/country/flag.tsx
+++ FE/components/country/flag.tsx
@@ -5,16 +5,15 @@ import Image from "next/image";
 type FlagProps = {
     src: string;
     alt: string;
+    itemClass: string;
 }
 
-export default function Flag({ src, alt }: FlagProps) {
+export default function Flag({ src, alt, itemClass }: FlagProps) {
     const validFlag = typeof src === 'string' && src.length;
 
     if (!validFlag) return null;
 
     return (
-        <div>
-            <Image src={src} height={200} width={200} alt={`${alt} flag`} />
-        </div>
+        <Image className={itemClass} src={src} height={200} width={200} alt={`${alt} flag`} />
     );
 }
